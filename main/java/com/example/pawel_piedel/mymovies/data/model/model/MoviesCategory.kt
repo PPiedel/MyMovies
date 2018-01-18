208a551ae30e122240426fa7b1761d75cb890a14
ppiedel <pawelpiedel@yahoo.pl>
2018-01-18 22:07:23 +0100

    Main, test, androidTest packages moved to right place.

--- main/java/com/example/pawel_piedel/mymovies/data/model/model/MoviesCategory.kt
+++
@@ -1,8 +0,0 @@
-package com.example.pawel_piedel.mymovies.data.model.model
-
-/**
- * Created by Pawel_Piedel on 30.10.2017.
- */
-enum class MoviesCategory {
-    TOP_RATED, UPCOMING, POPULAR, DEFAULT, NOW_PLAYING
-}
